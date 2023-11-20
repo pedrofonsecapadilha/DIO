51b716ca858039afbac67a1f9f9d65099a3e7e57
pedropadilha <pedrofpadilha@gmail.com>
2023-11-19 21:53:06 -0300

    Aualização de Estudos + Atividades

--- Avanade - Programação C# com CRM Dynamics/ExemploFundamentos/Program.cs
+++ Avanade - Programação C# com CRM Dynamics/ExemploFundamentos/Program.cs
@@ -1,5 +1,21 @@
 ﻿using ExemploFundamentos.Models;
 
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine("Data e hora atual: " + dataAtual.ToString("dd/MM/yyyy HH") + "h\n");
+
+string apresentacao = "Olá, seja bem vindx";
+int quantidade = 1;
+quantidade = 10;
+double altura = 1.80;
+decimal preco = 1.85M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preço: " + preco);
+Console.WriteLine("Valor da variável condição: " + condicao);
+
 Pessoa p = new Pessoa();
 
 p.Nome = "Pedro";
