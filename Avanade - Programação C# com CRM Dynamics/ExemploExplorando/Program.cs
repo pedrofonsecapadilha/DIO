45f36029a807cd2dcdb32c8c3d9a9c15a9bd5de4
pedropadilha <pedrofpadilha@gmail.com>
2023-11-22 18:53:51 -0300

    Atualização dos estudos e exercícios

---
+++ Avanade - Programação C# com CRM Dynamics/ExemploExplorando/Program.cs
@@ -0,0 +1,6 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Asdrubal";
+p1.Idade = 47;
+p1.Apresentar();
--- Avanade - Programação C# com CRM Dynamics/ExemploFundamentos/Program.cs
+++ Avanade - Programação C# com CRM Dynamics/ExemploFundamentos/Program.cs
@@ -1,18 +1,112 @@
-﻿// Exercícios - Operadores Aritiméticos em C#
+﻿// Exercícios - Array e Listas
 
-using ExemploFundamentos.Models;
+// int[] arrayInteiros = new int[3];
 
-Calculadora calc = new Calculadora();
+// arrayInteiros[0] = 100;
+// arrayInteiros[1] = 225;
+// arrayInteiros[2] = 375;
 
-calc.Somar(6,3);
-calc.Subtrair(6,3);
-calc.Multiplicar(6,3);
-calc.Dividir(6,3);
-calc.Potencia(6,3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
-calc.RaizQuadrada(36);
+// int[] arrayInteiros = { 100, 225, 354 };
+
+// // for(int count = 0; count < arrayInteiros.Length; count++)
+// // {
+// //     Console.WriteLine($"Posição: {count} - Valor: {arrayInteiros[count]}");
+// // }
+
+// foreach(int valor in arrayInteiros)
+// {
+//     Console.WriteLine(valor);
+// }
+
+List<string> exemplo = new List<string>();
+exemplo.Add("SP");
+exemplo.Add("MG");
+exemplo.Add("BA");
+
+foreach(string item in exemplo)
+{
+    Console.WriteLine(item);
+}
+
+// Exercícios - Conhecendo as Estruturas de Repetição em C#
+
+// int numero = 7;
+
+// for(int count = 0; count <= 10; count++)
+// {
+//     Console.WriteLine($"{numero} x {count} = {numero * count}");
+// }
+
+// int numero = 8;
+// int count = 0;
+
+// while (count <= 10)
+// {
+//     Console.WriteLine($"{numero} x {count} = {numero * count}");
+//     count++;
+// }
+
+// int sum = 0, num = 0;
+
+// do
+// {
+//     Console.WriteLine("Digite um número (0 para parar)");
+//     num = Convert.ToInt32(Console.ReadLine());
+
+//     sum += num;
+
+// } while(num != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {sum}");
+
+// string opcao;
+
+// while(true)
+// {
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+//         case "2":
+//             Console.WriteLine("Buscar cliente");
+//             break;
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             Environment.Exit(0);
+//             break;
+//         default:
+//             Console.WriteLine("Opção Inválida");
+//             break;
+//     }
+// }
+
+// Exercícios - Operadores Aritiméticos em C#
+
+// using ExemploFundamentos.Models;
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(6,3);
+// calc.Subtrair(6,3);
+// calc.Multiplicar(6,3);
+// calc.Dividir(6,3);
+// calc.Potencia(6,3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+// calc.RaizQuadrada(36);
 
 // Exercícios - Operadores Condicionais
 
